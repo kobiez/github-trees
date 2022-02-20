455baf39b4fb3ef59a1ddbcaa9d125ecb86436ad
Kobi Ezra <kobiye1@gmail.com>
2022-02-21 01:08:27 +0200

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GitHubTrees from './components/GitHubTrees';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub Libraries</h1>
+      <GitHubTrees />
+    </div>
+  );
+}
+
+export default App;
